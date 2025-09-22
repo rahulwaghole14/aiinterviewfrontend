9408a678bf074bfe0e8ee44537cc59aee0d3281c
mayur533 <mayurkhalate63@gmail.com>
2025-09-22 18:29:07 +0530

    Revert "fix: Fixed height, button layout, and spacing"
    This reverts commit cf7cca37cd9c8397ce817aaa5849043df700013c.

--- src/components/AddCandidates.jsx
+++ src/components/AddCandidates.jsx
@@ -519,7 +519,10 @@ const AddCandidates = () => {
               <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12 5V19M5 12H19" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
               </svg>
-              <span>Add Candidate</span>
+              <div className="button-text">
+                <span>Add</span>
+                <span>Candidate</span>
+              </div>
             </button>
             
             {/* Show Add Candidate form below when Add Candidate button is selected */}
